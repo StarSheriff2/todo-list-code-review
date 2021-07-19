fa4c2932aaa369660530359b128b216883530172
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2021-07-19 17:51:27 -0600

    refactor code into a function for easier readability

--- src/lib/task_status.js
+++ src/lib/task_status.js
@@ -1,17 +1,20 @@
 import toDoList from './tasks.js';
 
 const statusCheckboxChange = (e) => {
+  const taskCompletion = (taskTextDecoration, taskStatus, task) => {
+
+    taskTextDecoration = (taskStatus) ? 'line-through': '';
+
+    task.updateStatus(taskStatus);
+
+    localStorage.setObj('myToDoList', toDoList.toDoListArray);
+  };
+
   const itemId = parseInt(e.target.parentNode.id, 10);
-  if (e.target.checked) {
-    e.target.parentNode.children[1].style.textDecoration = 'line-through';
-    const task = toDoList.getTask(itemId);
-    task.updateStatus(true);
-  } else {
-    e.target.parentNode.children[1].style.textDecoration = '';
-    const task = toDoList.getTask(itemId);
-    task.updateStatus(false);
-  }
-  localStorage.setObj('myToDoList', toDoList.toDoListArray);
+  const task = toDoList.getTask(itemId);
+  const taskTextDecoration = e.target.parentNode.children[1].style.textDecoration;
+  const taskStatus = e.target.checked;
+  taskCompletion(taskTextDecoration, taskStatus, task);
 };
 
 export default statusCheckboxChange;
